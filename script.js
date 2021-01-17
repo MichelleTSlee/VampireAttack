a91124521d24c0c1250627c82564d0ca475de483
Michelle Slee <sleemichelle@gmail.com>
2021-01-17 11:03:47 +0000

    Added foreground

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ var spaceship = new Image();
 spaceship.src = "images/ship.png";
 var galaxy = new Image();
 galaxy.src = "images/galaxy.jpg";
+var foreground = new Image();
+foreground.src = "images/foreground.png";
 var laser = new Image();
 laser.src = "images/laser.png";
 
@@ -23,18 +25,18 @@ var beam1 = {
   x:100,
   y: 0,
   width: 64,
-  height: 220
+  height: 200
 }
 
 var beam2 = {
   x: 100,
   y: 230,
   width: 64,
-  height: 250
+  height: 270
 }
 
-// var gap = 75;
-// var constant = beam1.height + gap;
+var gap = 30;
+var constant = beam1.height + gap;
 
 var lives = 3;
 
@@ -62,12 +64,14 @@ document.addEventListener("keyup", function(event){
 spaceship.ready = false;
 galaxy.ready = false;
 laser.ready = false;
+foreground.ready = false;
 spaceship.onload = checkReady();
 
 //Check Ready & PlayGame
 function checkReady(){
    this.ready=true;
    galaxy.ready = true;
+   laser.ready = true;
    laser.ready = true;
    playGame();
  }
@@ -84,10 +88,11 @@ function render(){
 //NB Order matters here - draw galaxy first
   context.drawImage(galaxy, 0, 0);
 
+
 //This creates lasers at edge of screen & keeps moving them to the left
   for(var i = 0; i < beam.length; i++) {
     context.drawImage(laser, beam[i].x, beam[i].y, beam1.width, beam1.height);
-    context.drawImage(laser, beam[i].x, beam2.y, beam2.width, beam2.height);
+    context.drawImage(laser, beam[i].x, beam[i].y+constant, beam2.width, beam2.height);
     context.drawImage(spaceship, ship.x, ship.y, 50, 50);
     beam[i].x--;
 
@@ -99,7 +104,7 @@ function render(){
     }
   }
 
-//Currently only changing top beam. If using gap & constant for bottom beam y need to also calculate gap between bottom of beam2 & size of canvas & increase beam2 height (or decrease) by as much every turn
+context.drawImage(foreground, 0, 220, 305, 250);
 
 
   context.font = "15px Verdana";
